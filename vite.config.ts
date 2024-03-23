fa2948b1421db9538f3b5ac502b0009aa91fd07c
Sören Oldag <soeren_oldag@freenet.de>
2024-03-23 02:13:07 +0100

    Add github pages deployment to CI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/forest-shuffle-scoring/",
   plugins: [react()],
   resolve: {
     alias: {
