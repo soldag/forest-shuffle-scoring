4053fda79d1f74c6fc2dedb4d718d8b96befd393
Sören Oldag <soeren_oldag@freenet.de>
2024-03-30 18:08:12 +0100

    Parse value to number in CaveCardCountInput

--- src/components/common/CaveCardCountInput.tsx
+++ src/components/common/CaveCardCountInput.tsx
@@ -1,18 +1,40 @@
+import { useCallback } from "react";
+
 import StyleIcon from "@mui/icons-material/Style";
 import { Input, InputProps } from "@mui/joy";
 
-type CaveCardCountInputProps = Omit<
-  InputProps,
-  "startDecorator" | "slotProps" | "type"
->;
+interface CaveCardCountInputProps
+  extends Omit<
+    InputProps,
+    "startDecorator" | "slotProps" | "type" | "onChange" | "value"
+  > {
+  value: number | null;
+  onChange?: (value: number | null) => void;
+}
 
-const CaveCardCountInput: React.FC<CaveCardCountInputProps> = (props) => (
-  <Input
-    {...props}
-    type="number"
-    startDecorator={<StyleIcon />}
-    slotProps={{ input: { min: 0 } }}
-  />
-);
+const CaveCardCountInput: React.FC<CaveCardCountInputProps> = ({
+  value,
+  onChange,
+  ...remainingProps
+}) => {
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = parseInt(e.target.value, 10);
+      onChange?.(isNaN(value) ? null : value);
+    },
+    [onChange],
+  );
+
+  return (
+    <Input
+      {...remainingProps}
+      type="number"
+      startDecorator={<StyleIcon />}
+      slotProps={{ input: { min: 0 } }}
+      value={value === null ? "" : value}
+      onChange={handleChange}
+    />
+  );
+};
 
 export default CaveCardCountInput;
