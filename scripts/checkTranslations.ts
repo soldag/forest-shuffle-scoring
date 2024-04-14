15eddbcaaa62917b53256abce5bcc8d9c2f559be
Sören Oldag <soeren_oldag@freenet.de>
2024-04-14 18:26:10 +0200

    Improve checkTranslations script
    * Check for message order
    * Separate missing and empty messages
    * Add severity levels
    * Add fix option

--- scripts/checkTranslations.ts
+++ scripts/checkTranslations.ts
@@ -1,10 +1,14 @@
-import "colors";
+import colors from "colors";
 import fs from "fs";
 import { glob } from "glob";
+import _ from "lodash";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const ROOT_DIR = path.resolve(fileURLToPath(import.meta.url), "../..");
+const ROOT_DIR = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "..",
+);
 const TRANSLATIONS_DIR = path.resolve(ROOT_DIR, "src/translations");
 const FILE_GLOB = path.resolve(TRANSLATIONS_DIR, "*.json");
 const DEFAULT_LOCALE_FILE_PATH = path.resolve(TRANSLATIONS_DIR, "en.json");
@@ -12,29 +16,97 @@ const DEFAULT_LOCALE_FILE_PATH = path.resolve(TRANSLATIONS_DIR, "en.json");
 const readMessages = (filename: string) =>
   fs.existsSync(filename) ? JSON.parse(fs.readFileSync(filename, "utf-8")) : {};
 
-let hasErrors = false;
+const checkMessages = (
+  defaultMessageIds: string[],
+  messages: { [key: string]: string },
+) => {
+  const issues = [];
+  const messageIds = Object.keys(messages);
+
+  const missingIds = defaultMessageIds.filter((id) => !messageIds.includes(id));
+  missingIds
+    .sort()
+    .forEach((id) => issues.push({ code: "missing-id", description: id }));
+
+  const unusedIds = messageIds.filter((id) => !defaultMessageIds.includes(id));
+  unusedIds
+    .sort()
+    .forEach((id) => issues.push({ code: "unused-id", description: id }));
+
+  const emptyIds = Object.entries(messages)
+    .filter(([, m]) => m.length === 0)
+    .map(([k]) => k);
+  emptyIds
+    .sort()
+    .forEach((id) =>
+      issues.push({ code: "empty-message", description: id, level: "warning" }),
+    );
+
+  const isOrdered = _.isEqual(
+    messageIds,
+    defaultMessageIds.filter((id) => !missingIds.includes(id)),
+  );
+  if (!isOrdered) {
+    issues.push({
+      code: "wrong-order",
+      description: "The messages are not ordered properly",
+      level: "warning",
+    });
+  }
+
+  return issues;
+};
+
+const fix = process.argv.includes("--fix");
 const defaultMessageIds = Object.keys(readMessages(DEFAULT_LOCALE_FILE_PATH));
 
-glob.sync(FILE_GLOB).forEach((filePath) => {
-  if (filePath === DEFAULT_LOCALE_FILE_PATH) return;
+const results = glob.sync(FILE_GLOB).map((filePath) => {
+  if (filePath === DEFAULT_LOCALE_FILE_PATH) return true;
 
-  const messages = readMessages(filePath);
-  const messageIds = Object.keys(messages);
-  const missingIds = defaultMessageIds.filter((k) => !messages[k]);
-  const unusedKeys = messageIds.filter((k) => !defaultMessageIds.includes(k));
-  if (missingIds.length + unusedKeys.length > 0) {
-    hasErrors = true;
+  let messages = readMessages(filePath);
+  let issues = checkMessages(defaultMessageIds, messages);
 
+  let numFixedIssues = 0;
+  if (fix && issues.length > 0) {
+    const newMessages = Object.fromEntries(
+      defaultMessageIds.map((key) => [key, messages[key] || ""]),
+    );
+    fs.writeFileSync(filePath, JSON.stringify(newMessages, null, 2));
+
+    const newIssues = checkMessages(defaultMessageIds, newMessages);
+    numFixedIssues = issues.filter((i1) =>
+      newIssues.every((i2) => !_.isEqual(i1, i2)),
+    ).length;
+
+    messages = newMessages;
+    issues = newIssues;
+  }
+
+  if (issues.length > 0 || numFixedIssues > 0) {
     const relativeFilePath = path.relative(ROOT_DIR, filePath);
     console.error(relativeFilePath.underline);
-    missingIds
-      .sort()
-      .forEach((id) => console.log(`${"missing-id".red}  ${id}`));
-    unusedKeys
-      .sort()
-      .forEach((id) => console.log(`${"unused-id".red}   ${id}`));
-    console.error();
+
+    if (issues.length > 0) {
+      const maxCodeLength = Math.max(...issues.map((x) => x.code.length));
+      for (const issue of _.orderBy(issues, ["code"])) {
+        const { code, description, level = "error" } = issue;
+        const color = level === "error" ? colors.red : colors.yellow;
+        console.log(`${color(code.padEnd(maxCodeLength))}  ${description}`);
+      }
+      console.log();
+    }
+
+    if (numFixedIssues > 0) {
+      const message =
+        numFixedIssues === 1
+          ? `${numFixedIssues} issue has been fixed automatically.`
+          : `${numFixedIssues} issues have been fixed automatically.`;
+      console.log(message.green);
+      console.log();
+    }
   }
+
+  return issues.length === 0;
 });
 
-process.exit(hasErrors ? 1 : 0);
+process.exit(results.every((x) => x) ? 0 : 1);
