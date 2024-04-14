827d81ff384c6a8b043ac5b80e4df5d9895c7205
Sören Oldag <soeren_oldag@freenet.de>
2024-04-14 15:50:41 +0200

    Add back button to ScoringView footer

--- src/components/views/ScoringView/components/Footer.tsx
+++ src/components/views/ScoringView/components/Footer.tsx
@@ -2,23 +2,40 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import { Link } from "wouter";
 
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ReplayIcon from "@mui/icons-material/Replay";
-import { Button } from "@mui/joy";
+import { Button, Stack } from "@mui/joy";
 
 const Footer: React.FC = () => (
-  <Button
-    fullWidth
-    variant="soft"
-    color="neutral"
-    startDecorator={<ReplayIcon />}
-    component={Link}
-    to="/new"
+  <Stack
+    direction="row"
+    alignItems="center"
+    justifyContent="space-around"
+    gap={2}
   >
-    <FormattedMessage
-      id="ScoringView.Footer.newGame"
-      defaultMessage="Score a new game"
-    />
-  </Button>
+    <Button
+      fullWidth
+      variant="soft"
+      color="neutral"
+      startDecorator={<ArrowBackIcon />}
+      onClick={() => history.back()}
+    >
+      <FormattedMessage id="ScoringView.Footer.back" defaultMessage="Go back" />
+    </Button>
+    <Button
+      fullWidth
+      variant="soft"
+      color="neutral"
+      startDecorator={<ReplayIcon />}
+      component={Link}
+      to="/new"
+    >
+      <FormattedMessage
+        id="ScoringView.Footer.newGame"
+        defaultMessage="New game"
+      />
+    </Button>
+  </Stack>
 );
 
 export default Footer;
