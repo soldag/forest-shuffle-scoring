bda7ea169c157383e9057f6924b19e462890465e
Sören Oldag <soeren_oldag@freenet.de>
2025-06-10 01:01:55 +0200

    Remove promo card icon from CardButton

--- src/components/common/CardButton.tsx
+++ src/components/common/CardButton.tsx
@@ -1,9 +1,8 @@
 import { useIntl } from "react-intl";
 
-import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import { Button, ButtonProps } from "@mui/joy";
 
-import { Card, CardType, GameBox } from "@/game";
+import { Card, CardType } from "@/game";
 import {
   ACTIVE_BRIGHTNESS,
   HOVER_BRIGHTNESS,
@@ -25,12 +24,10 @@ const getBackground = (card: Card, adjustBrightness: number = 0) => {
 
 const CardButton = ({ card, ...otherProps }: CardButtonProps) => {
   const intl = useIntl();
-  const isPromoCard = card.gameBox === GameBox.PromoCards;
 
   return (
     <Button
       {...otherProps}
-      endDecorator={isPromoCard && <AutoAwesomeIcon />}
       slotProps={{
         endDecorator: {
           sx: { position: "absolute", right: "0.75rem" },
