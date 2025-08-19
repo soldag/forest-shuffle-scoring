3aab5cc4ee33481374266db2a0b77caa4c5574f9
Sören Oldag <soeren_oldag@freenet.de>
2025-08-19 02:40:26 +0200

    Enforce locale type checks
    This makes it easier to identify code that needs updates when introducing a new language.

--- src/components/common/LocaleSelect.tsx
+++ src/components/common/LocaleSelect.tsx
@@ -4,30 +4,34 @@ import { Option, Select, SelectProps, Typography } from "@mui/joy";
 
 import { Locale } from "@/types";
 
-const LOCALES = _.orderBy(
-  [
-    {
-      code: "de",
-      flag: "🇩🇪",
-      label: "Deutsch",
-    },
-    {
-      code: "en",
-      flag: "🇺🇸",
-      label: "English",
-    },
-    {
-      code: "nl",
-      flag: "🇳🇱",
-      label: "Nederlands",
-    },
-  ],
+interface LocaleOptions {
+  flag: string;
+  label: string;
+}
+
+const OPTIONS: Record<Locale, LocaleOptions> = {
+  [Locale.De]: {
+    flag: "🇩🇪",
+    label: "Deutsch",
+  },
+  [Locale.En]: {
+    flag: "🇺🇸",
+    label: "English",
+  },
+  [Locale.Nl]: {
+    flag: "🇳🇱",
+    label: "Nederlands",
+  },
+};
+
+const SORTED_OPTIONS = _.orderBy(
+  Object.entries(OPTIONS).map(([code, options]) => ({ code, ...options })),
   (l) => l.label,
 );
 
 const LocaleSelect = (props: SelectProps<Locale, false>) => (
   <Select {...props}>
-    {LOCALES.map(({ code, flag, label }) => (
+    {SORTED_OPTIONS.map(({ code, flag, label }) => (
       <Option key={code} value={code} label={flag}>
         <Typography startDecorator={flag}>{label}</Typography>
       </Option>
--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,5 +1,13 @@
+import { Locale } from "@/types";
+
 import de from "./de.json";
 import en from "./en.json";
 import nl from "./nl.json";
 
-export default { en, de, nl };
+const translations: Record<Locale, Record<string, string>> = {
+  [Locale.De]: de,
+  [Locale.En]: en,
+  [Locale.Nl]: nl,
+};
+
+export default translations;
