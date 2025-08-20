95e64a924187bc2e47ebb28af66d9a127b154f5b
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2025-08-20 16:47:06 -0300

    update file name

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -3,7 +3,7 @@ import { Locale } from "@/types";
 import de from "./de.json";
 import en from "./en.json";
 import nl from "./nl.json";
-import ptBr from "./ptbr.json";
+import ptBr from "./pt-br.json";
 
 const translations: Record<Locale, Record<string, string>> = {
   [Locale.De]: de,
