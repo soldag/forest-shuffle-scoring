6839e49da01ea2ffa40fba3daacc417ae0ce198a
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2025-08-20 15:16:19 -0300

    add pt-br to locale

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -3,11 +3,13 @@ import { Locale } from "@/types";
 import de from "./de.json";
 import en from "./en.json";
 import nl from "./nl.json";
+import ptBr from "./ptbr.json";
 
 const translations: Record<Locale, Record<string, string>> = {
   [Locale.De]: de,
   [Locale.En]: en,
   [Locale.Nl]: nl,
+  [Locale.PtBr]: ptBr,
 };
 
 export default translations;
